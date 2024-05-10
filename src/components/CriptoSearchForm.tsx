298fba4320ab9a70b8204ba51163976a1f58b8f5
DaniM0613 <wwh.rj.92@gmail.com>
2024-05-10 15:14:36 -0600

    agregando los datos

--- src/components/CriptoSearchForm.tsx
+++ src/components/CriptoSearchForm.tsx
@@ -1,4 +1,4 @@
-
+import { currencies } from "../data"
 
 
 export default function CriptoSearchForm() {
@@ -14,14 +14,17 @@ export default function CriptoSearchForm() {
             id="currency"
          >
            <option value=''>-- Seleccione --</option>
+           {currencies.map( currency => (
+              <option key={currency.code} value={currency.code}>{currency.name}</option>
+           ))}
          </select>
       </div>
 
       <div className="field">
-         <label htmlFor="Criptomoneda">Criptomoneda: </label>
+         <label htmlFor="criptocurrency">Criptomoneda: </label>
          <select
-            name="Criptomoneda"
-            id="Criptomoneda"
+            name="criptocurrency"
+            id="criptocurrency"
          >
            <option value=''>-- Seleccione --</option>
          </select>
---
+++ src/types/index.ts
@@ -0,0 +1,4 @@
+import { z } from 'zod'
+import { type CurrencySchema } from '../schema/crypto-schema'
+
+export type Currency = z.infer<typeof CurrencySchema>
