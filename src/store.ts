de8f3b7915d5898674f3a61b3f6544c5ca25a1a4
DaniM0613 <wwh.rj.92@gmail.com>
2024-05-31 19:05:54 -0600

    mostrando la respuesta en el state

--- src/store.ts
+++ src/store.ts
@@ -1,16 +1,25 @@
 import { create } from 'zustand'
 import { devtools} from 'zustand/middleware'
-import { Cryptocurrency, Pair } from './types'
+import { Cryptocurrency, Pair, CryptoPrice } from './types'
 import { getCryptos, fetchCurrentCryptoPrice} from './services/CryptoService'
 
 type CryptoStore = {
     cryptocurrencies: Cryptocurrency[]
+    result: CryptoPrice
     fetchCryptos: () => Promise<void>
     fetchData: (pair : Pair) => Promise<void>
 }
 
 export const useCryptoStore =  create<CryptoStore>()(devtools((set) => ({
     cryptocurrencies: [],
+    result: {
+        IMAGEURL : '',
+        PRICE : '',
+        HIGHDAY : '',
+        LOWDAY : '',
+        CHANGEPCT24HOUR: '',
+        LASTUPDATE: ''
+    } ,
     fetchCryptos: async () => {
         const cryptocurrencies = await getCryptos()
         set(()=> ({
@@ -18,6 +27,9 @@ export const useCryptoStore =  create<CryptoStore>()(devtools((set) => ({
         }))
     },
     fetchData: async (pair) => {
-        fetchCurrentCryptoPrice(pair)
+        const result = await fetchCurrentCryptoPrice(pair)
+        set(() => ({
+            result
+        }))
     }
 })))
